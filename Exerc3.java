b73cdf7d982b6ac266c920ead663622df370c7a8
Matheus_Neves <matheuswdn@gmail.com>
2024-06-05 17:24:56 -0300

    Entrega de exercicios

---
+++ Exerc3.java
@@ -0,0 +1,10 @@
+package JavaBasico;
+
+public class Exerc3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OlaMundo.java
@@ -0,0 +1,10 @@
+package JavaBasico;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
