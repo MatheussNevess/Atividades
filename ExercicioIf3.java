3a8ef349beb8cb58f357f8bf6891c37a76e0b4f6
Matheus_Neves <matheuswdn@gmail.com>
2024-06-06 14:13:38 -0300

    Exercicios laço de repetição

--- ExercicioIf3.java
+++ ExercicioIf3.java
@@ -10,26 +10,26 @@ public class ExercicioIf3 {
 		int idade;
 		String nome;
 		boolean doacao;
-		
+
 		Scanner leia = new Scanner(System.in);
-		
+
 		System.out.println("\nDigite seu nome: ");
 		nome = leia.nextLine();
 		System.out.println("\nDigite sua idade: ");
 		idade = leia.nextInt();
 		System.out.println("\nÉ a primeira doação de sangue? (true/false): ");
 		doacao = leia.nextBoolean();
-		
-		if(idade >= 18 && idade <60) {
-			System.out.printf("\n%s está apto para doar sangue.",nome);
-		}else if(idade >=60 && idade <70 && doacao == false) {
-			System.out.printf("\n%s está apto para doar sangue.",nome);
-		}else if(idade >=60 && idade <70 && doacao == true){
-			System.out.printf("\n%s está não está apto para doar sangue.",nome);
-		}else {
-			System.out.printf("\n%s está não está apto para doar sangue.",nome);
+
+		if (idade >= 18 && idade < 60) {
+			System.out.printf("\n%s está apto para doar sangue.", nome);
+		} else if (idade >= 60 && idade < 70 && doacao == false) {
+			System.out.printf("\n%s está apto para doar sangue.", nome);
+		} else if (idade >= 60 && idade < 70 && doacao == true) {
+			System.out.printf("\n%s está não está apto para doar sangue.", nome);
+		} else {
+			System.out.printf("\n%s está não está apto para doar sangue.", nome);
 		}
-				
+
 	}
 
 }
